f94c8e40d578bfc887f828561f631a81a22a6309
Jens Jasche <jens.jasche@fysik.su.se>
2018-09-25 15:21:22 +0200

    added script to trust notebooks

---
+++ make_trust.py
@@ -0,0 +1,7 @@
+import os
+os.system('jupyter trust Galaxy_chronicles.ipynb')
+os.system('jupyter trust wien_displacement.ipynb')
+os.system('jupyter trust Tstars.ipynb')
+os.system('jupyter trust hr.ipynb')
+os.system('jupyter trust Interlude.ipynb')
+os.system('jupyter trust habitable_zone.ipynb')
